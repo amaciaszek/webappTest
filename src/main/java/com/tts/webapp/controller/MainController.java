dd7c2f96422964046f0341634e4d6a6541c2e33a
unknown <amaciaszek@umass.edu>
2020-08-03 10:17:49 -0400

    first one

---
+++ src/main/java/com/tts/webapp/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.tts.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	@RequestMapping("/")
+	public String home() {
+		return "main";
+	}	
+}
